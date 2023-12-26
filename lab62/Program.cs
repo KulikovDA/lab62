433c06dec09d3137905366df1a51adc00dd51e1b
Arion <Arion@DESKTOP-9BU80RS>
2023-12-26 15:12:22 +0200

    Добавьте файлы проекта.

---
+++ lab62/Program.cs
@@ -0,0 +1,33 @@
+﻿using lib62;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Введите количество строк: ");
+        int rows = int.Parse(Console.ReadLine());
+
+        Console.Write("Введите количество столбцов: ");
+        int cols = int.Parse(Console.ReadLine());
+
+        int[,] matrix = new int[rows, cols];
+
+        Console.WriteLine("Введите элементы матрицы:");
+        for (int i = 0; i < rows; i++)
+        {
+            for (int j = 0; j < cols; j++)
+            {
+                Console.Write("Элемент [{0},{1}] = ", i, j);
+                matrix[i, j] = int.Parse(Console.ReadLine());
+            }
+        }
+
+        int[] columnSums = lib.CalculateColumnSums(matrix);
+
+        Console.WriteLine("Результат:");
+        foreach (int sum in columnSums)
+        {
+            Console.Write(sum + " ");
+        }
+    }
+}
---
+++ lab62/lib62.cs
@@ -0,0 +1,23 @@
+﻿namespace lib62
+{
+    public class lib
+    {
+        public static int[] CalculateColumnSums(int[,] matrix)
+        {
+            int rows = matrix.GetLength(0);
+            int cols = matrix.GetLength(1);
+
+            int[] columnSums = new int[cols];
+
+            for (int j = 0; j < cols; j++)
+            {
+                for (int i = 0; i < rows; i++)
+                {
+                    columnSums[j] += matrix[i, j];
+                }
+            }
+
+            return columnSums;
+        }
+    }
+}
